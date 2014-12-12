16a33244a97b15ee81f87ecd65ce68f41259436d
tuskyk <tusharkrishnan@gmail.com>
2014-12-11 20:11:43 -0500

    Sorted some 'bugs'

--- bugclassversion_two_particles_with_noise.m
+++ bugclassversion_two_particles_with_noise.m
@@ -20,18 +20,21 @@ tstop = 200;
 tvec = t:DT:tstop;
 
 
+food_lambda = [510, 700];
+[R1,G1,B1]= wavelength_to_RGB(food_lambda(1));
+[R2, G2, B2]= wavelength_to_RGB(food_lambda(2));     
 
-%Food color
-food_lambda = (750-380)*rand() + 380; % Food color
-% Conversion of color to RGB values
-[R, G, B] = wavelength_to_RGB(food_lambda);      
 
 %Make Food
-tgt = [150*rand(1,2)-[75. 75.]]; %Food target location
+tgt = [150*rand(2)-[75. 75.; 75. 75.]]; %food target location
 f_center = tgt;
 f_width = 5;
-food = rectangle('Position',[f_center(1)-f_width(1)/2,f_center(2)-f_width(1)/2,f_width(1),f_width(1)],'Curvature',[0.5,0.5],'EdgeColor',[R, G, B], 'LineWidth', 4);
-set(food,'Position',[f_center(1)-f_width(1)/2,f_center(2)-f_width(1)/2,f_width(1),f_width(1)],'Curvature',[0.5,0.5]);
+
+%Green Food
+food_1 = rectangle('Position',[f_center(1,1)-f_width(1)/2,f_center(1,2)-f_width(1)/2,f_width(1),f_width(1)],'Curvature',[0.5,0.5],'EdgeColor',[R1, G1, B1],'LineWidth', 4);
+
+%Red Food
+food_2 = rectangle('Position',[f_center(2,1)-f_width(1)/2,f_center(2,2)-f_width(1)/2,f_width(1),f_width(1)],'Curvature',[0.5,0.5],'EdgeColor',[R2, G2, B2], 'LineWidth', 4);
 
 heading_angle=0;
 
@@ -43,36 +46,49 @@ yR = -5;
 xL = 10;
 yL = 5;
 
-
-% Sense color with added noise
+%Sense the color of the food particles with noise
 food_lambda_noise = poissrnd(food_lambda);
-colorAll = color_sense(food_lambda_noise); 
-
+colorAll = color_sense(food_lambda_noise);  
+ 
 % Compute the magnitude of the sensor info
-sensorL = sqrt((xL - f_center(1)).^2 + (yL - f_center(2)).^2);
-sensorR = sqrt((xR - f_center(1)).^2 + (yR - f_center(2)).^2);
+sensorL(1) = sqrt((xL - f_center(1,1)).^2 + (yL - f_center(1,2)).^2);
+sensorR(1) = sqrt((xR - f_center(1,1)).^2 + (yR - f_center(1,2)).^2);
+
+sensorL(2)= sqrt((xL - f_center(2,1)).^2 + (yL - f_center(2,2)).^2);
+sensorR(2)= sqrt((xR - f_center(2,1)).^2 + (yR - f_center(2,2)).^2);
+
 draw_robot(x,y, heading_angle);
 
-last_food = 0; % counter for changing food location and color
 for k=1:length(tvec) 
     
-    %Brain function implementation
+    %Brain function
     [motorL, motorR] = brain(sensorL, sensorR, colorAll);
 
-    %Compute left and right velocities
-    vel_left = motorL / 40;   
-    vel_right = motorR / 40;
+    %Compute the ;eft and right velocities
+    vel_left = motorL / 50;  
+    vel_right = motorR / 50;
     
     heading_angle = heading_angle + atan(((vel_right - vel_left)) / 10); % update heading angle and x,y location of robot
     
     x = x + (vel_left + vel_right)./2 * DT * cos(heading_angle); %change x direction
     y = y + (vel_left + vel_right)./2 * DT * sin(heading_angle); %change y direction
 
-% Compute the magnitude of the sensor info 
-    sensorL = sqrt((xL - f_center(1)).^2 + (yL - f_center(2)).^2);
-    sensorR = sqrt((xR - f_center(1)).^2 + (yR - f_center(2)).^2);
+  % Identify the new X and Y coordinate of the R and L sensor from the
+    % robot description
+    xR = new_verts(1);   
+    yR = new_verts(14);
+    xL = new_verts(11);
+    yL = new_verts(24);
+     
+% Compute the magnitude of the sensor info for both foods 
+    sensorL(1) = sqrt((xL - f_center(1,1)).^2 + (yL - f_center(1,2)).^2);
+    sensorR(1) = sqrt((xR - f_center(1,1)).^2 + (yR - f_center(1,2)).^2);
 
-    if x>arena_size % have arena wrap on itself
+    sensorL(2)= sqrt((xL - f_center(2,1)).^2 + (yL - f_center(2,2)).^2);
+    sensorR(2)= sqrt((xR - f_center(2,1)).^2 + (yR - f_center(2,2)).^2);
+
+    %Arena wraps on itself
+    if x>arena_size
         x=-arena_size;
     end
     if y>arena_size
@@ -88,35 +104,32 @@ for k=1:length(tvec)
     draw_robot(x,y, heading_angle);
     drawnow
     
+  
+    DL = sqrt((x - f_center(1,1)).^2 + (y - f_center(1,2)).^2);
     
-    % Identify the new X and Y coordinate of the R and L sensor from the
-    % robot description
-    xR = new_verts(1);   
-    yR = new_verts(14);
-    xL = new_verts(11);
-    yL = new_verts(24);
-    
-    DL = sqrt((x - f_center(1)).^2 + (y - f_center(2)).^2);
-    
-    %Get new food particle if eaten or
-    %too much time has elapsed
-    if DL < 10 || (k-last_food>200 && colorAll(6)>0.5)
+    %Creating new green food on it getting eaten
+    if DL < 10  
         
-        last_food = k; % Timer is updated 
         tgt = [150*rand(1,2)-[75. 75.]]; % New food position
-        food_lambda = (750-380)*rand() + 380; % New food color 
-        [R, G, B] = wavelength_to_RGB(food_lambda); 
-
-        f_center = tgt;
-        set(food,'Position',[f_center(1)-f_width(1)/2,f_center(2)-f_width(1)/2,f_width(1),f_width(1)],'Curvature',[0.5,0.5], 'EdgeColor', [R, G, B]);
+        f_center(1,:) = tgt;
+        set(food_1,'Position',[f_center(1,1)-f_width(1)/2,f_center(1,2)-f_width(1)/2,f_width(1),f_width(1)],'Curvature',[0.5,0.5]);
         
-        food_lambda_noise = poissrnd(food_lambda);
-        colorAll = color_sense(food_lambda_noise); % New neural input 
-                
+        food_lambda_noise(1) = poissrnd(food_lambda(1)); 
+        colorAll = color_sense(food_lambda_noise); 
     end
+    
+    % Creating red food at a new location after a certain time duration
+    if mod(k,500) == 0 
+        
+        tgt = [150*rand(1,2)-[75. 75.]]; % New food position
+        f_center(2,:) = tgt;
+        set(food_2,'Position',[f_center(2,1)-f_width(1)/2,f_center(2,2)-f_width(1)/2,f_width(1),f_width(1)],'Curvature',[0.5,0.5]);
+        
+        food_lambda_noise(2) = poissrnd(food_lambda(2));
+        colorAll = color_sense(food_lambda_noise); 
+    end
+            
 end
-
-
 
 function draw_robot(x,y,heading_angle)
 persistent nverts npts verts linewidth color hrobot
@@ -196,20 +209,29 @@ function output = color_sense(lambda)
     output = color_net([n1; n2; n3; n4;]); 
     
 function [motorL, motorR] = brain(sensorL, sensorR, colorAll)
-  color_names = {'Violet', 'Blue', 'Green', 'Yellow', 'Orange', 'Red'}; % Categories, for reference
-  thresh = 0.5; % threshold for detection
+   
+  %Average out sensed information from left and right sensors
+  sensor_avg(1) = (sensorL(1) + sensorR(1))/2;
+  sensor_avg(2) = (sensorL(2) + sensorR(2))/2;
   
-  % Compute a relationship of sensor input to motor output
-  
-  %Behaviour on detecting danger aka red - Coward
-  if(colorAll(6)>thresh)
-   motorL = 300 - sensorL; 
-   motorR = 300 - sensorR; 
-   title('Ahh!! DANGER', 'FontSize', 16)
-   return
+  %Check for closest food particle and accordingly act
+  if sensor_avg(1) > sensor_avg(2)
+       if sensorL(2) < sensorR(2)
+           motorL = 300 - sensorL(2); 
+           motorR = 290 - sensorR(2);
+       else
+           motorL = 290 - sensorL(2); 
+           motorR = 300 - sensorR(2);
+       end
+       title('Get away from Red!!', 'FontSize', 16);
+  else
+       motorL = 300 - sensorR(1);
+       motorR = 300 - sensorL(1);
+       title('Go for Green!!', 'FontSize', 16);
   end
   
-  % Behaviour on detecting food - Aggressor
-  title('Yay!! Food.', 'FontSize', 16)
-  motorL = 300 - sensorR;   
-  motorR = 300 - sensorL;
+  
+  
+  
+
+  
